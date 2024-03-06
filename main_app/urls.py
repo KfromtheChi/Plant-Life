1c8af10f302104f47fe6deff0b4e7ad31bff412f
K Castillo <krystalin.castillo@gmail.com>
2024-03-06 17:32:52 -0600

    About page URL defined - root route, that will display the about page, mapped.

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     # this root route will display the home page
     path('', views.home, name='home'),
+    # this route will display the about page
+    path('about/', views.about, name='about'),
 ]
