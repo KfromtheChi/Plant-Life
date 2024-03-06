6fe967de24781b549782a995c4ee258f747910cf
K Castillo <krystalin.castillo@gmail.com>
2024-03-06 16:09:55 -0600

    Include function imported.

--- plant_app/urls.py
+++ plant_app/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # route defined in main_app included here
     path('', include('main_app.urls')),
 ]
