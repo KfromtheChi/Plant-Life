b20cb121b6f8aa9a96af7e665283dfb5b267bfbd
K Castillo <krystalin.castillo@gmail.com>
2024-03-06 17:35:37 -0600

    About page view function defined.

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 # Define the home view
 def home(request):
     return render(request, 'home.html') # The .html file extension renders the home page template
+# Define the about view
+def about(request):
+    return render(request, 'about.html') # The .html file extension renders the about page template
