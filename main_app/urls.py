ae9d68fd9554c4336984c5a8a332f4a9fb6c61b5
K Castillo <krystalin.castillo@gmail.com>
2024-03-06 17:10:24 -0600

    Home Page view function defined, render template shortcut imported.

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 	
 urlpatterns = [
+    # this root route will display the home page
     path('', views.home, name='home'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Define the home view
+def home(request):
+    return render(request, 'home.html') # The .html file extension renders the home page template
