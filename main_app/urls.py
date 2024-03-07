d75fada7f8bf0f67155a8de313cc4950b91353d9
K Castillo <krystalin.castillo@gmail.com>
2024-03-07 00:54:41 -0600

    Quiz UI, URL route, and view created/defined/coded.

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     # this route will display the about page
     path('about/', views.about, name='about'),
-    path('plants/', views.plants_index, name='index')
+    path('quiz/', views.quiz, name='quiz'),
+    path('plants/', views.plants, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -8,8 +8,13 @@ def home(request):
 # Define the about view
 def about(request):
     return render(request, 'about.html')
+# Define the quiz view
+def quiz(request):
+    return render(request, 'quiz/quiz.html', {
+        'quiz': quiz
+    })
 # Define the plants view
-def plants_index(request):
+def plants(request):
     return render(request, 'plants/index.html', {
         'plants': plants
     })
