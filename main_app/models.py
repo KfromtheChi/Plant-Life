445a8b807551696aae197f6fb0345bc480f2f01f
K Castillo <krystalin.castillo@gmail.com>
2024-03-07 05:04:39 -0600

    Plant model created.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Plant(models.Model):
+    name = models.TextField()
+    species = models.TextField()
+    botanical_name = models.TextField()
+    notes = models.TextField()
