6a955d42b686ff0d0c8da4805c5b365180c5829d
K Castillo <krystalin.castillo@gmail.com>
2024-03-07 12:33:23 -0600

    Quiz form changed, inputs added and drop down oprtion removed, quiz model created and imported to views.py.

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,19 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+
+class Quiz(models.Model):
+    question1 = models.TextField()
+    question2 = models.TextField()
+    question3 = models.TextField()
+    question4 = models.TextField()
+
+    def __str__(self):
+        return self.question
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'quiz_id': self.id})
+
 class Plant(models.Model):
     name = models.TextField()
     species = models.TextField()
