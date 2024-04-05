9d83924d997e5f8f831f68d7131ce95764cf3cff
K Castillo <krystalin.castillo@gmail.com>
2024-04-05 02:22:45 -0500

    Add plant care route added to urls.py and action attribute added to the plant_care_form.

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,9 +9,11 @@ urlpatterns = [
     path('care/', views.care_tips, name='tips'),
     path('plants/', views.plants_index, name='index'),
     path('plants/<int:plant_id>/', views.plants_detail, name='detail'),
+    path('plants/<int:plant_id/add_plant_care/', views.add_plant_care, name='add_care'),
     path('plants/create/', views.PlantCreate.as_view(), name='create'),
     path('plants/<int:pk>/update/', views.PlantUpdate.as_view(), name='update'),
     path('plants/<int:pk>/delete/', views.PlantDelete.as_view(), name='delete'),
     path('quiz/', views.quiz, name='quiz'),
     path('submit_quiz/', views.SubmitQuiz.as_view(), name='submit_quiz'),
+
 ]
